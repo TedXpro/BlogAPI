cb1c0a2ea521f3f9ce3f312e3cb53cda75b544aa
TedXpro <johannes.woldeyes@gmail.com>
2025-01-12 07:56:59 +0300

    work on the Controllers/BlogController.cs file to handle update and delete operations

--- BlogApi/Controllers/BlogController.cs
+++ BlogApi/Controllers/BlogController.cs
@@ -17,7 +17,37 @@ namespace BlogApi.Controllers{
             return Ok(blogs);
         }
 
-        // [HttpGet("{id}")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetBlog(string id){
+            var blog = await _blogService?.GetBlog(id)!;
+            if(blog != null){
+                return Ok(blog);
+            }
+            return NotFound($"There is No blog with id => {id}");
+        }
 
+        [HttpPost]
+        public async Task<ActionResult> CreateBlog(Blog blog){
+            if(await _blogService?.CreateBlog(blog)! == true){
+                return Ok("Blog Created Successfully");
+            }
+            return BadRequest("Failed to create Blog");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateBlog(string id, Blog blog){
+            if(await _blogService?.UpdateBlog(id, blog)! == true){
+                return Ok("Blog Updated Successfully"); 
+            }
+            return NotFound($"There is No blog with id => {id}");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBlog(string id){
+            if(await _blogService?.DeleteBlog(id)! == true){
+                return Ok("Blog Deleted Successfully");
+            }
+            return NotFound($"There is No blog with id => {id}");
+        }
     }
 }
--- BlogApi/Services/BlogService.cs
+++ BlogApi/Services/BlogService.cs
@@ -5,9 +5,9 @@ namespace BlogApi.Services{
     public class BlogService : IBlogService{
         private readonly IMongoCollection<Blog>? _blogs;
         public BlogService(IConfiguration configuration){
-            var client = new MongoClient(configuration["MongoDbSettings:ConnectionString"]);
-            var database = client.GetDatabase(configuration["MongoDbSettings:DatabaseName"]);
-            _blogs = database.GetCollection<Blog>(configuration["MongoDbSettings:BlogCollectionName"]); 
+            var client = new MongoClient(configuration["MonogoDbSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["MonogoDbSettings:DatabaseName"]);
+            _blogs = database.GetCollection<Blog>(configuration["MonogoDbSettings:BlogCollectionName"]); 
         }
 
         public async Task<List<Blog>> GetBlogs() {
@@ -21,11 +21,13 @@ namespace BlogApi.Services{
         }
             
         public async Task<bool> CreateBlog(Blog blog){
+            blog.Id = null;
             await _blogs?.InsertOneAsync(blog)!;
             return true;
         }
 
         public async Task<bool> UpdateBlog(string id, Blog blog){
+            blog.Id = id;
             var status = await _blogs.ReplaceOneAsync(b => b.Id == id, blog);
             if(status.ModifiedCount > 0){
                 return true;
